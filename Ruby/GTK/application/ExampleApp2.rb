2b7a18115f466495c4713a57bd5a698349382822
Madh93 <michelangelo93@gmail.com>
2015-10-29 21:31:39 +0000

    More Gtk

---
+++ Ruby/GTK/application/ExampleApp2.rb
@@ -0,0 +1,73 @@
+#! /usr/bin/env ruby
+
+require 'gtk3'
+require "fileutils"
+
+
+# Load a resource file
+# see here https://developer.gnome.org/gio/stable/GResource.html
+current_path = File.expand_path(File.dirname(__FILE__))
+gresource_bin = "#{current_path}/exampleapp.gresource"
+gresource_xml = "#{current_path}/exampleapp.gresource.xml"
+
+# Generate the resource file:
+# see here https://developer.gnome.org/gio/stable/glib-compile-resources.html
+system("glib-compile-resources",
+       "--target", gresource_bin,
+       "--sourcedir", File.dirname(gresource_xml),
+       gresource_xml)
+
+at_exit do
+  FileUtils.rm_f(gresource_bin)
+end
+
+resource = Gio::Resource.load(gresource_bin)
+Gio::Resources.register(resource)
+
+
+class ExampleAppWindow < Gtk::ApplicationWindow
+  type_register # Method inherited from GLib::Object
+
+  class << self
+    def init
+      set_template(resource: "/org/gtk/exampleapp/window.ui")
+    end
+  end
+
+  def initalize(application)
+    super(application: application)
+  end
+
+  def open(file)
+  end
+end
+
+class ExampleApp < Gtk::Application
+  def initialize
+    super("org.gtk.exampleapp", :handles_open)
+
+    signal_connect "activate" do |application|
+      window = ExampleAppWindow.new(application: application)
+      window.present
+    end
+    signal_connect "open" do |application, files, hin|
+      windows = application.windows
+      win = nil
+      unless windows.empty?
+        win = windows.first
+      else
+        win = ExampleAppWindow.new(application)
+      end
+
+      files.each { |file| win.open(file) }
+
+      win.present
+    end
+  end
+end
+
+app = ExampleApp.new
+
+
+# puts app.run([$0] + ARGV)
+puts app.run
